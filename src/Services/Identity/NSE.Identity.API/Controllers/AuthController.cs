61778421ae379457ddaae1fa9e440c0efd1558ee
Christian Oliveira <christianoliveira1908@hotmail.com>
2026-04-18 12:46:57 -0300

    Configurando registro e login de usuário.

---
+++ src/Services/Identity/NSE.Identity.API/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identity.API.Models;
+
+namespace NSE.Identity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Register(UserRegister userRegister)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = userRegister.Email,
+                Email = userRegister.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, userRegister.Password);
+
+            if (result.Succeeded) {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(userLogin.Email, userLogin.Password, false, true);
+
+            if (result.Succeeded) {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+    }
+}
