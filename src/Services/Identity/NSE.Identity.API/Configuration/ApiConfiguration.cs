9bf2de2444a62b692e640a63ad8c51f3b1307111
Christian Oliveira <christianoliveira1908@hotmail.com>
2026-04-18 13:14:43 -0300

    Criado tabelas do Identity na base de dados via migration.

--- src/Services/Identity/NSE.Identity.API/Configuration/ApiConfiguration.cs
+++ src/Services/Identity/NSE.Identity.API/Configuration/ApiConfiguration.cs
@@ -4,12 +4,27 @@
     {
         public static WebApplicationBuilder AddApiConfiguration(this WebApplicationBuilder builder)
         {
+            builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+            var connectionString = builder.Configuration["DefaultConnection"];
+
             builder.Services
-                .AddApplicationDbContext(builder.Configuration["DefaultConnection"])
+                .AddApplicationDbContext(connectionString)
                 .AddIdentity()
                 .AddControllers(); // Retorna um IMvcBuilder, logo não da para chamar o AddOpenApi
 
             builder.Services.AddOpenApi();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "NerdStore Enterprise Identity API",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact() { Email = "oliveirachristian1908@gmail.com", Name = "Christian Bueno"},
+                    License = new Microsoft.OpenApi.Models.OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
 
             return builder;
         }
@@ -19,7 +34,13 @@
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
             }
 
             app.UseHttpsRedirection()
