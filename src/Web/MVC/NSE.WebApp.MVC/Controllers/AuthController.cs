ec766a5842c79bd4e8ab402ca18b6341fb7e9c7e
Christian Oliveira <christianoliveira1908@hotmail.com>
2026-05-03 13:53:32 -0300

    Criado base para a controller/view de autenticação

--- src/Web/MVC/NSE.WebApp.MVC/Controllers/AuthController.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Controllers/AuthController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<ActionResult> Register(UserRegister userRegister)
         {
+            if (!ModelState.IsValid) return View(userRegister);
+
+
             return View();
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        [Route("login")]
+        public ActionResult Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return View(userLogin);
+
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public ActionResult Logout() {
+            return RedirectToAction("Index", nameof(HomeController).Replace("Controller", ""));
         }
     }
 }
---
+++ src/Web/MVC/NSE.WebApp.MVC/Models/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.WebApp.MVC.Models
+{
+    public class UserRegister
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} a {1} caracteres.", MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} a {1} caracteres.", MinimumLength = 5)]
+        public string PasswordConfirm { get; set; }
+    }
+
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} a {1} caracteres.", MinimumLength = 5)]
+        public string Password { get; set; }
+    }
+}
